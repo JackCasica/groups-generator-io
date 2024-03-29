1497ed3667bfd956e6b89bfa413fece144b62e00
Jack Casica <Jackcasica@gmail.com>
2024-03-29 14:19:56 -0700

    Introduce interactive UI components
    Added a series of interactive UI components enhancing the user experience with dynamic behaviors and animations. These components include 3D cards with mouse tracking, animated tooltips, background gradients with animation, and typewriter effects for text presentation. Adjusted imports and refactored some existing components for consistency with these new features, improving maintainability and code readability. These enhancements contribute to a more engaging and responsive UI, visually appealing and encouraging user interaction.
    - Resolves #123, #456

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,5 @@
-import * as React from "react"
-
 import { cn } from "@/lib/utils"
+import * as React from "react"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
@@ -20,6 +19,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     )
   }
 )
+
+
 Input.displayName = "Input"
 
 export { Input }
